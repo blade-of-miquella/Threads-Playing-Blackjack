0ddf13754034bbdaca7b18286d40591c461db619
blade-of-miquella <melinaprogg@gmail.com>
2025-04-26 08:25:25 -0800

    ver 0.1 raw basic setup

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <thread>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+vector<int> cards = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+void main() {
+	random_device rd;
+	mt19937 g(rd());
+}
