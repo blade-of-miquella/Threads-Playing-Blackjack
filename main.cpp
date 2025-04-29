fc9ec98d5fcf5b617b8b03300c4a448c552ea40c
blade-of-miquella <melinaprogg@gmail.com>
2025-04-29 09:29:24 -0800

    ver 1.0

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-const int ROUNDS = 10;
+const int ROUNDS = 1000;
 
 vector<int> cards_template = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
                               2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
@@ -42,9 +42,31 @@ void player_pick(vector<int>& player_cards, bool& current_done, bool& previous_d
     for (int round = 1; round <= ROUNDS; ++round) {
         unique_lock<mutex> lock(mtx);
         cv.wait(lock, [&] { return previous_done; });
-        if (cards.size() >= 5) {
-            player_cards.assign(cards.begin(), cards.begin() + 5);
-            cards.erase(cards.begin(), cards.begin() + 5);
+
+        player_cards.push_back(dealer_cards[0]);
+        player_cards.push_back(dealer_cards[1]);
+        dealer_cards.erase(dealer_cards.begin(), dealer_cards.begin() + 2);
+
+        int it = 0;
+        while (true) {
+            int sum = accumulate(player_cards.begin(), player_cards.end(), 0);
+            if (sum == 21 || sum == 20 || sum > 21) break;
+            else if (sum <= 15) {
+                player_cards.push_back(dealer_cards[it]);
+                dealer_cards.erase(dealer_cards.begin() + it);
+                it++;
+                continue;
+            }
+            else if (sum <= 17 && rand() % 8 == 2) {
+                player_cards.push_back(dealer_cards[it]);
+                dealer_cards.erase(dealer_cards.begin() + it);
+                break;
+            }
+            else if (sum <= 19 && rand() % 15 == 4) {
+                player_cards.push_back(dealer_cards[it]);
+                dealer_cards.erase(dealer_cards.begin() + it);
+                break;
+            }
         }
         current_done = true;
         previous_done = false;
@@ -52,9 +74,9 @@ void player_pick(vector<int>& player_cards, bool& current_done, bool& previous_d
     }
 }
 
-
 void write_results() {
     ofstream outfile("results.txt");
+    int player1Rounds = 0, player2Rounds = 0, draws = 0;
     for (int round = 1; round <= ROUNDS; ++round) {
         unique_lock<mutex> lock(mtx);
         cv.wait(lock, [] { return player2_done; });
@@ -62,7 +84,7 @@ void write_results() {
         int player2_sum = accumulate(player2_cards.begin(), player2_cards.end(), 0);
         outfile << "Round " << round << ":\n";
         outfile << "Dealer shuffled cards: ";
-        for (int card : dealer_cards) {
+        for (int card : cards) {
             outfile << card << " ";
         }
         outfile << "\nPlayer 1 cards: ";
@@ -73,24 +95,40 @@ void write_results() {
         for (int card : player2_cards) {
             outfile << card << " ";
         }
-        if (player1_sum > player2_sum) {
+        if ((player1_sum > player2_sum || player2_sum > 21) && player1_sum <= 21) {
             outfile << "\nPlayer 1 wins!\n\n";
+            player1Rounds++;
         }
-        else if (player1_sum < player2_sum) {
+        else if ((player1_sum < player2_sum || player1_sum > 21) && player2_sum <= 21) {
             outfile << "\nPlayer 2 wins!\n\n";
+            player2Rounds++;
         }
         else {
             outfile << "\nIt's a draw!\n\n";
+            draws++;
         }
+
+        player1_cards.clear();
+        player2_cards.clear();
+
         player2_done = false;
         write_done = true;
         cv.notify_all();
     }
+    outfile << "Player 1 win Rounds: " << player1Rounds << "\n";
+    outfile << "Player 2 win Rounds: " << player2Rounds << "\n";
+    outfile << "Calculating...\n";
+    if (player1Rounds > player2Rounds) outfile << "Player 1 Win!\n";
+    else if (player1Rounds < player2Rounds) outfile << "Player 2 Win!\n";
+    else outfile << "Draw!\n";
+    outfile << "Number of draws: " << draws;
 
     outfile.close();
 }
 
 int main() {
+    srand(time(0));
+
     thread t1(shuffle_cards);
     thread t2(player_pick, ref(player1_cards), ref(player1_done), ref(shuffle_done), 1);
     thread t3(player_pick, ref(player2_cards), ref(player2_done), ref(player1_done), 2);
